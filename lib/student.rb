589795346d8b1ed4640aa14f57cfc9876b129267
Sean Beach <sean.beach1@yahoo.com>
2019-12-30 12:51:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,13 @@
 class Student < User
 
-  attr_accessor :knowledge
+    attr_accessor :knowledge
 
-  def initialize
-    @knowledge = []
-  end
+    def initialize
+        @knowledge = []
+    end
 
-  def learn(knowledge)
-    @knowledge << knowledge
-  end
+    def learn(string)
+        self.knowledge << string
+    end
 
-  def knowledge
-    @knowledge
-  end
-end
+end 
